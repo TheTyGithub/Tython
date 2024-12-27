cb5a28c163abff86609c38d9433e653e5c7976dc
TheTyGithub <167589574+TheTyGithub@users.noreply.github.com>
2024-12-27 23:13:51 +0000

    Not indenting but works

--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import RosBasic
 
 while True:
     text = input('RosBasic > ')
-    result, error = RosBasic.run(text)
+    result, error = RosBasic.run('<stdin>', text)
 
     if error: print(error.as_string())
     else: print(result)
